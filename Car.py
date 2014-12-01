f00f93f869a8005eb61e82b5b3dd19441c86efa3
Matthias Jaenicke <m.a.jaenicke@gmail.com>
2014-12-01 13:52:21 +0100

    Can I has graphix?
    Renders black screen and red rectangle for car.

--- Car.py
+++ Car.py
@@ -3,13 +3,16 @@ Created on 27.06.2014
 
 @author: Matthias
 '''
+from OpenGL.GL import *
+from pygame.locals import *
+    
+import pygame
 
 class Car(object):
     '''
-    represents a car with all essentiel parts.
+    represents a car with all essential parts.
     What did YOU think this is?!
     '''
-
 
     def __init__(self, xPos, yPos, horsePower, weight, color):
         '''
@@ -23,6 +26,9 @@ class Car(object):
         self.__color        = color
         self.__speed        = 0.0
         self.__direction    = 0.0 #direction in degrees
+        
+        self.width          = 20
+        self.length         = 50
     
     # power between 0.0 and 1.0    
     def accelerate(self, timeSlice, power=1.0):
@@ -33,3 +39,12 @@ class Car(object):
     def brake(self, timeSlice):
         #TODO: depends on time and weight...
         pass
+    
+    def render(self):
+        glBegin(GL_QUADS)
+        glColor4fv(self.__color)
+        glVertex3f(self.__xPos - self.width/2, self.__yPos - self.length / 2, 0.0)
+        glVertex3f(self.__xPos + self.width/2, self.__yPos - self.length / 2, 0.0)
+        glVertex3f(self.__xPos + self.width/2, self.__yPos + self.length / 2, 0.0)
+        glVertex3f(self.__xPos - self.width/2, self.__yPos + self.length / 2, 0.0)
+        glEnd()
--- hcnd_v1.py
+++ hcnd_v1.py
@@ -3,9 +3,63 @@ Created on 24.06.2014
 
 @author: teh_nicKLess
 '''
+from OpenGL.GL import *
+from pygame.locals import *
 
+import pygame
 
+from Car import Car
 
+screenSize = 800, 600
 
+carStartPos = screenSize[0]/2, screenSize[1]/2
+car = Car( carStartPos[0], carStartPos[1], 0, 0, pygame.Color(255,0,0).normalize())
+
+def resize((width, height)):
+    if height == 0:
+        height = 1
+    glViewport(0, 0, width, height)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    glOrtho(-10.0, width + 10.0, height + 10.0, -10.0, -6.0, 0.0)
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity
+
+def init():
+    glClearColor(0.0, 0.0, 0.0, 0.0)
+    
+    
+def render():
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glLoadIdentity()
+    
+    
+    
+    glBegin(GL_LINES)
+    glColor3f(1.0, 1.0, 0.0)
+    glVertex3f(1.0, 0.0, 0.0)
+    glVertex3f(1.0, 20.0, 0.0)
+    glEnd()
+    
+    car.render()
+    
+    pygame.display.flip()
+
+def main():
+    video_flags = OPENGL | HWSURFACE | DOUBLEBUF
+    
+    pygame.init()
+    pygame.display.set_mode(screenSize, video_flags)
+    
+    resize(screenSize)
+    init()
+    
+    while True:
+        event = pygame.event.poll()
+        if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+            break
+        
+        render()
+        
 if __name__ == '__main__':
-    pass
+    main()
